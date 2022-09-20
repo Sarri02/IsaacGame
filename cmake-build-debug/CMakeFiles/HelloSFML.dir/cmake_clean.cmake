39bf24958ade518b60d4976cee518d4f82161fba
Sarri02 <marco.sarri@stud.unifi.it>
2022-09-20 09:49:49 +0200

    20/09/2022

--- cmake-build-debug/CMakeFiles/HelloSFML.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/HelloSFML.dir/cmake_clean.cmake
@@ -1,5 +1,4 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/HelloSFML.dir/Bullet.cpp.obj"
   "CMakeFiles/HelloSFML.dir/Character.cpp.obj"
   "CMakeFiles/HelloSFML.dir/Door.cpp.obj"
   "CMakeFiles/HelloSFML.dir/Floor.cpp.obj"
